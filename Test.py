72346c31d567b492c53034948d3ede825a33770a
shresth577 <41500558+shresth577@users.noreply.github.com>
2022-03-01 22:51:45 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,17 @@
+
+#! /usr/bin/python3
+import unittest
+
+from Prog1 import summation
+
+class TestSum(unittest.TestCase):
+	def test_list_int(self):
+		"""
+		Test case to add 2 nos
+		"""
+		data={23,22}
+		result=summation(data)
+		self.assertEqual(result,55)
+
+if __name__ == '__main__':
+	unittest.main()		
